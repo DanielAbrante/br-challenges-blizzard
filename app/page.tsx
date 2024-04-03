403139d4e237e9e264ceeeddc5f3e2b60ab91340
Daniel Abrante <danielabrante@protonmail.com>
2024-04-03 09:00:24 -0300

    style: Adjust border bottom from menu to higher resolutions

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,17 @@ export default function Home() {
   return (
     <>
       <Menu />
-    
+
       <section className="relative pt-20">
         <div>
-          <Image src={background} alt="Background" className="absolute -z-10 top-0 h-full object-cover" />
+          <Image src={background} alt="Background" className="absolute -z-10 top-0 h-full w-full object-cover" />
         </div>
-        <h1 className="text-[2.5rem]">Retorne à escuridão com o game Diablo IV</h1>
-        <p>O retorno de Lilith traz uma era de escuridão e sofrimento</p>
-        <button>Jogue Agora</button>
+
+        <div>
+          <h1 className="text-[2.5rem]">Retorne à escuridão com o game Diablo IV</h1>
+          <p>O retorno de Lilith traz uma era de escuridão e sofrimento</p>
+          <button>Jogue Agora</button>
+        </div>
       </section>
     </>
   )
--- app/ui/components/Menu.tsx
+++ app/ui/components/Menu.tsx
@@ -5,11 +5,13 @@ import menuIcon from "@/public/assets/banner-hero/icons/menu.svg";
 
 export default function Menu() {
   return (
-    <header className="absolute w-full flex justify-between p-6 border-b-[1px] border-white/10 ">
-      <a href="#" className="after:content-[''] after:absolute after:w-11 after:h-[2px] after:-bottom-[1.5px] after:bg-blue after:pointer-events-none">
-        <Image src={logo} alt="Blizzard Company" className="max-w-16" />
-      </a>
-      <Image src={menuIcon} alt="Menu icon toggle" />
-    </header>
+    <div className="border-b-[1px] border-white/10 absolute w-full">
+      <header className="max-w-[1280px] mx-auto flex justify-between p-6 z-10">
+        <a href="#" className="after:content-[''] after:absolute after:w-11 after:h-[2px] after:-bottom-[1.5px] after:bg-blue after:pointer-events-none">
+          <Image src={logo} alt="Blizzard Company" className="max-w-16" />
+        </a>
+        <Image src={menuIcon} alt="Menu icon toggle" className="hover:cursor-pointer" />
+      </header>
+    </div>
   )
 }
