94b05acd0af7547651f5bcd5b28c68b5272d92f2
Daniel Abrante <danielabrante@protonmail.com>
2024-04-03 09:12:32 -0300

    style: add responsive to blizzard logo and hidden menu on higher resolutions

--- app/ui/components/Menu.tsx
+++ app/ui/components/Menu.tsx
@@ -5,12 +5,12 @@ import menuIcon from "@/public/assets/banner-hero/icons/menu.svg";
 
 export default function Menu() {
   return (
-    <div className="border-b-[1px] border-white/10 absolute w-full">
-      <header className="max-w-[1280px] mx-auto flex justify-between p-6 z-10">
+    <div className="border-b-[1px] border-white/10 absolute w-full z-10">
+      <header className="max-w-[1280px] mx-auto flex justify-between p-6">
         <a href="#" className="after:content-[''] after:absolute after:w-11 after:h-[2px] after:-bottom-[1.5px] after:bg-blue after:pointer-events-none">
-          <Image src={logo} alt="Blizzard Company" className="max-w-16" />
+          <Image src={logo} alt="Blizzard Company" className="max-w-16 sm:max-w-20 xl:max-w-28" />
         </a>
-        <Image src={menuIcon} alt="Menu icon toggle" className="hover:cursor-pointer" />
+        <Image src={menuIcon} alt="Menu icon toggle" className="hover:cursor-pointer xl:hidden" />
       </header>
     </div>
   )
