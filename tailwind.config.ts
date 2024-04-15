5b0ebb0a2b651f239a0453977b1bd6fe5143a3e9
Daniel Abrante <danielabrante@protonmail.com>
2024-04-15 08:54:45 -0300

    style: add responsive screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        tablet: "768px",
+        laptop: "1440px",
+        desktop: "1920px",
+      },
       colors: {
         blue: "#00AEFF",
+        black: "#020203",
+        white: "#FFFFFF",
+        gray: "#8F9199",
+        grayLight: "#E5E5E5",
       },
       fontSize: {
         bannerTitle: "clamp(2.5rem, 1rem + 6vw, 4rem);",
+      },
+      gridTemplateColumns: {
+        smallGrid: "repeat(2, minmax(156px, 1fr))",
+        mediumGrid: "repeat(3, minmax(203px, 1fr))",
+        bigGrid: "repeat(4, minmax(280px, 301px))",
       },
     },
   },
