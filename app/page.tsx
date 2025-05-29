c610847795442a120f146aefcc3ee5a9b04df605
Daniel Abrante <danielabrante@protonmail.com>
2025-05-29 09:14:29 -0300

    t

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Banner from "./sections/Banner";
 import Footer from "./sections/Footer";
 import Gallery from "./sections/Gallery";
 import Menu from "./sections/Header";
 
 export default function Home() {
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div>
+				<h1 className="text-blue">Loading...</h1>
+			</div>
+		);
+	}
+
 	return (
 		<>
 			<Menu />
